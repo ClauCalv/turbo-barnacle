d852ba1a18f0209a1651212361ecdfeab854221c
felipenery <49323293+felipenery@users.noreply.github.com>
2020-03-17 20:30:52 -0300

    Update TextoNegrito.java

--- ModuloConfigurador/app/src/main/java/ufabc/projeto/moduloconfigurador/TextoNegrito.java
+++ ModuloConfigurador/app/src/main/java/ufabc/projeto/moduloconfigurador/TextoNegrito.java
@@ -1,23 +1,35 @@
 package ufabc.projeto.moduloconfigurador;
 
 import android.content.Intent;
-import android.graphics.Typeface;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Vibrator;
+import android.speech.tts.TextToSpeech;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.util.Locale;
+import me.anwarshahriar.calligrapher.Calligrapher;
 
-public class TextoNegrito extends AppCompatActivity {
+public class TextoNegrito extends ConfigAll implements TextToSpeech.OnInitListener {
 
     private Button simBotao;
     private Button naoBotao;
     private Button testeBotao;
-    private TextView Tv;
     private Button mostrarBotao;
 
-
+    //Cor do fundo e texto
+    private int corTela;
+    private int corText;
+    private LinearLayout linearLayout;
+    private TextView Tv;
+    private String fonte;
+    //TTS mostrando a velocidade e tom
+    private TextToSpeech textToSpeech;
+    public Boolean teste;
+    public float testeVelocidadeFala;
+    public float testeTomFala;
 
 
     @Override
@@ -30,15 +42,39 @@ public class TextoNegrito extends AppCompatActivity {
         naoBotao = (Button) findViewById(R.id.naoId);
         testeBotao = (Button) findViewById(R.id.testeId);
         mostrarBotao = (Button) findViewById(R.id.mostrarId);
+        linearLayout = (LinearLayout) findViewById(R.id.layoutId);
 
+        //cor texto e fundo
+        if(sharedPreferences.getInt(KEY_COR_TELA, corTela) != 0) {
+            corTela = sharedPreferences.getInt(KEY_COR_TELA, corTela);
+            linearLayout.setBackgroundColor(corTela);
+        }
+        if (sharedPreferences.getInt(KEY_COR_TEXTO, corText) != 0){
+        corText = sharedPreferences.getInt(KEY_COR_TEXTO, corText);
+        Tv.setTextColor(corText);
+        }
+        //som
+        teste = ((ConfigHelper) this.getApplication()).getAtivarOLeitorTela();
+        testeVelocidadeFala = ((ConfigHelper) this.getApplication()).getVelocidadeFala();
+        testeTomFala = ((ConfigHelper) this.getApplication()).getTomFala();
 
-
+        if (teste != false) {
+            textToSpeech = new TextToSpeech(this,  this);
+        }
 
         simBotao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Typeface boldTypeface = Typeface.defaultFromStyle(Typeface.BOLD);
-                simBotao.setTypeface(boldTypeface);
+                Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                long milliseconds = 350;
+                vibrator.vibrate(milliseconds);
+
+                Calligrapher calligrapher = new Calligrapher(TextoNegrito.this);
+                calligrapher.setFont(TextoNegrito.this, "futura-extra-bold-italic.ttf", true);
+                Log.i("@testeNegrito", "NegritoTexto - "+calligrapher);
+                fonte = "futura-extra-bold-italic.ttf";
+                click(fonte);
+
                 startActivity(new Intent(TextoNegrito.this,ConfTamanhoFonteTexto.class));
             }
         });
@@ -46,30 +82,42 @@ public class TextoNegrito extends AppCompatActivity {
         naoBotao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Typeface boldTypeface = Typeface.defaultFromStyle(Typeface.BOLD);
-                naoBotao.setTypeface(boldTypeface);
+                Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+                long milliseconds = 350;
+                vibrator.vibrate(milliseconds);
+                fonte = null;
+                click(fonte);
                 startActivity(new Intent(TextoNegrito.this,ConfTamanhoFonteTexto.class));
-                startActivityForResult(new Intent(Settings.ACTION_DISPLAY_SETTINGS),0);
+               // startActivityForResult(new Intent(Settings.ACTION_DISPLAY_SETTINGS),0); teste de ir para as configurações direto do celular
             }
         });
+
 
         testeBotao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Typeface boldTypeface = Typeface.defaultFromStyle(Typeface.BOLD);
-                Tv.setTypeface(boldTypeface);
+                Calligrapher calligrapher = new Calligrapher(TextoNegrito.this);
+                calligrapher.setFont(TextoNegrito.this, "futura-extra-bold-italic.ttf", true);
 
             }
         });
 
-        mostrarBotao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(TextoNegrito.this,AlterarContraste.class));
-                startActivityForResult(new Intent(Settings.ACTION_DISPLAY_SETTINGS),0);
+    }
 
-            }
-        });
+    public void click(String fonte){
+        //Para armazenar um valor
+        sharedPreferences.edit().putString(KEY_NEGRITO, fonte).commit();
+    }
+    @Override
+    public void onInit(int status) { //FALANDO assim que abre a atividade
+        String falar = "Deseja colocar o texto em negrito"+ "\n Botão de teste" + "\n Botão sim" + "\n Botão não";
+        if (status != TextToSpeech.ERROR) {
+            textToSpeech.setLanguage(Locale.getDefault());
+            textToSpeech.setSpeechRate(testeVelocidadeFala);
+            textToSpeech.setPitch(testeTomFala);
+            textToSpeech.speak(falar, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+
     }
 
 }
